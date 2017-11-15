1291f704e07d8b8aa28e539ee423e76fc55cbed2
bojunf <33677111+bojunf@users.noreply.github.com>
2017-11-15 12:53:50 -0500

    Add files via upload

---
+++ lr-nl/LoopLogistic.m
@@ -0,0 +1,26 @@
+function [] = LoopLogistic(Trainfile, Testfile, LoopObj, LoopArray, otherValue, method)
+    
+    Acc = zeros(length(LoopArray), 1)
+    AccTest = zeros(length(LoopArray), 1)
+    
+    if strcmp(LoopObj, 'alpha')
+        for i = 1:length(LoopArray)
+            alpha = LoopArray(i)
+            [Acc(i), AccTest(i)] = Logistic(Trainfile, Testfile, alpha, otherValue, method)
+        end
+        output = [LoopArray Acc AccTest]
+        fname = sprintf('result-borm/Acc_alpha_%s.txt', method)
+        save(fname, 'output', '-ascii')
+    elseif strcmp(LoopObj, 'niter')
+        for i = 1:length(LoopArray)
+            niter = LoopArray(i)
+            [Acc(i), AccTest(i)] = Logistic(Trainfile, Testfile, otherValue, niter, method)
+        end
+        output = [LoopArray Acc AccTest]
+        fname = sprintf('result-borm/Acc_niter_%s.txt', method)
+        save(fname, 'output', '-ascii')
+    else
+        disp('Wrong loop object')
+        return
+    end
+end
